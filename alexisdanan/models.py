88989dfd4bf3f74a28085d43d3e324dc1f9b7364
Cheik-Siramakan Keita <cheiksiramakankeita@gmail.com>
2020-01-12 12:53:38 +0100

    ...

---
+++ alexisdanan/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Utilisateur(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    telephone = models.CharField(max_length=10, null=True)
+
+class Nouvelle(models.Model):
+    auteur = models.ForeignKey(User, related_name='+', on_delete=models.PROTECT)
+    titre = models.CharField(max_length=200, null=True)
+    contenu = models.TextField()
+    image = models.FileField(upload_to='uploads/')
+    date_de_publication = dateajout = models.DateTimeField(auto_now=True)
+
+class Message():
+    nom = models.CharField(max_length=50, null=False)
+    objet = models.CharField(max_length=200, null=False)
+    email = models.CharField(max_length=50, null=False)
+    message = models.TextField(null=False)
