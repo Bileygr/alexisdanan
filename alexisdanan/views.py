d9e01b761a03f5af9cf6d48cbc426a8d0ca14c7b
Cheik-Siramakan Keita <cheiksiramakankeita@gmail.com>
2020-01-12 11:33:30 +0100

    ...

--- alexisdanan/views.py
+++ alexisdanan/views.py
@@ -24,7 +24,7 @@ def contact(request):
     return render(request, 'contact.html')
 
 def liensutile(request):
-    return render(request, 'index.html')
+    return render(request, 'liens_utile.html')
 
 def comitesenfrance(request):
     return render(request, 'comites_en_france.html')
